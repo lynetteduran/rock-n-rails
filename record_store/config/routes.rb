b919461b4edc0bcb6c9da009f4e03601294b0017
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-10-17 10:58:17 -0700

    adds get record endpoint to config/routes.rb

--- record_store/config/routes.rb
+++ record_store/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get "/records" => "records#index", as: 'records' # add me!
 end
