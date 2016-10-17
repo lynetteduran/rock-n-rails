b8eee65da7065ae63e8c87f61c64833c6648f03b
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-10-17 11:54:01 -0700

    adds show.html.erb file to views to display record by id

--- record_store/config/routes.rb
+++ record_store/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	#http verb+path, controller#action
-	get "/records", to: "records#index", as: 'records'
-	get "/records/:id", to: "records#show", as: 'record'
+	get "/records", 	to: "records#index", as: 'records'
+	get "/records/:id", to: "records#show",  as: 'record'
+	
 end
